176c874b36500315cf3fceee6c5a7c0347a04b59
Muhammed Basam K <mohdbasamoffl@gmail.com>
2026-09-25 12:10:43 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Demo!");
+
+        int result = add(10, 20);
+        System.out.println("Result: " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
